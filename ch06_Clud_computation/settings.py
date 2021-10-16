10888d6b296a0c15b08bee01dd10321e08fde903
Sandipan Ghosh <ghoshm21@gmail.com>
2021-10-16 16:17:37 +0530

    Delete settings.py

--- ch06_Clud_computation/settings.py
+++
@@ -1,8 +0,0 @@
-region_name = "ap-south-1"
-aws_access_key_id = "AKIAVEMONQVNQA72QG2Q"
-aws_secret_access_key = "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"
-# --------------------------------------------------------------------- #
-twee_access_token = "97245681-WU4O8CqzsKMIZtpaMD6JbktrNO5Zq2zCzidYaMcSA"
-twee_access_token_secret = "8ZOhAtxv37IotpCSuGDBVWRvFZJBZbHkboTU7w5lT5Wvw"
-twee_consumer_key = "UrXdWsKMmBbuPKcD7xzbvjhOW"
-twee_consumer_secret = "HmN8bmio4GwDvqEoc88CzoPixJ4xdivzUX3snq259NGvFu3hq4"
